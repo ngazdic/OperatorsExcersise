90e4755fadaa0978663484e1365bd288e25a770b
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-07-06 13:04:55 -0400

    commiting the changes on the file

---
+++ OperatorExcersise/Calculations.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace OperatorExcersise
+{
+    public class Calculations
+    {
+
+
+        public static int Subtract(int num1, int num2)
+        {
+
+            return num1 - num2;
+
+
+        }
+
+
+        public static int Add(int num1, int num2)
+        {
+
+            return num1 + num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+
+            return num1 * num2;
+
+
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+
+
+            return num1 / num2;
+
+
+        }
+
+        public static int Modulus(int num1, int num2)
+        {
+
+            return num1 % num2;
+
+
+        }
+
+        public static double AreaOfCircle(double r)
+        {
+
+
+            double radius = Math.PI* Math.Pow(r, 2);
+
+            return radius;
+
+
+        }
+
+    }
+}
+
+
+
+    
+
---
+++ OperatorExcersise/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using OperatorExcersise;
+
+
+
+
+Console.WriteLine(Calculations.Subtract(2, 5));
+Console.WriteLine(Calculations.Multiply(2, 5));
+Console.WriteLine(Calculations.Divide(2, 2));
+Console.WriteLine(Calculations.Add(2, 5));
+Console.WriteLine(Calculations.Modulus(2, 2));
+
+
+
+
+
+
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int quotient = a / b;
+int remainder = a % b;
+
+if (a % b == 0)
+{
+
+    Console.WriteLine(a + "/" + b + " is " + quotient);
+}
+else
+{
+    Console.WriteLine($"{a} / {b} result is {quotient} reminder is {remainder}");
+}
+
+
+
+Console.WriteLine(" What is the radius of your circle ?");
+var radius = double.Parse(Console.ReadLine());
+Console.WriteLine($"The area of a circle with radius of {radius} is {Calculations.AreaOfCircle(radius)}");
+
